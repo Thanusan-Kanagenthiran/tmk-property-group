0539ba0e323410bfe0e7146fa62cb2b23db0a930
Thanusan-Kanagenthiran <thanushmk6@gmail.com>
2024-07-30 15:14:41 +0530

    feat: project-initialization

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "TMK Property Group",
+  description: "TMK Property Group is a property management system",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
