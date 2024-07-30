4ae2ff9dfd394671300bd38e4c13dec6b6412660
Thanusan-Kanagenthiran <thanushmk6@gmail.com>
2024-07-30 16:14:50 +0530

    project configuration : mui integration + sass install

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,4 @@
+import styles from "./home.module.scss";
 export default function Home() {
-  return <main>TMK property group</main>;
+  return <main className={styles.main}>TMK property group</main>;
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "@/styles/theme";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +20,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            {children}
+          </ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
     </html>
   );
 }
