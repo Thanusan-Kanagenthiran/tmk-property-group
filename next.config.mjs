c8f18fb9ef179b039c5abf121f02ec966c924bae
Thanusan-Kanagenthiran <thanushmk6@gmail.com>
2024-08-17 02:12:16 +0530

    R&D image upload dependency & next config for image

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["res.cloudinary.com"]
+  },
   async redirects() {
     return [
       {
         source: "/",
         destination: "/home",
-        permanent: true,
-      },
+        permanent: true
+      }
     ];
-  },
+  }
 };
 
 export default nextConfig;
